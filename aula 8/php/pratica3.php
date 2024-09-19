549ecc5e43fa908e08b6b2675041eeada4abe151
Andrei Cardozo <andrei.cardozo@unidavi.edu.br>
2024-09-19 20:15:54 -0300

    aula PHP

--- aula 8/php/pratica3.php
+++ aula 8/php/pratica3.php
@@ -1,24 +1,25 @@
 <?php
-    $SALARIO1 = 1000; 
+    $SALARIO1 = 1000;
     $SALARIO2 = 2000; 
     $SALARIO2 = $SALARIO1;
     $SALARIO2++;
     $SALARIO1 += $SALARIO1 * 0.10;
 
-    echo "Valor Salário 1: " . $SALARIO1 . " e Valor Salário 2: " . $SALARIO2;
+    echo "Valor do Salário 1 é: " . $SALARIO1 . " e o Valor do Salário 2 é: " . $SALARIO2 . "<br>";
+    echo " ";
 
     if ($SALARIO1 > $SALARIO2) {
-        echo "O valor da variável 1 é maior que o valor da variável 2";
+        echo "O valor da variável 1 é maior que o valor da variável 2 <br>";
     } else if ($SALARIO1 < $SALARIO2) {
-        echo "O valor da variável 2 é maior que o valor da variável 1";
+        echo "O valor da variável 2 é maior que o valor da variável 1 <br>";
     } else {
-        echo "Os valores são iguais";
+        echo "Os valores são iguais <br>";
     }
 
-        //comando de repetição while
-        $status = array('ótimo', 'Muito bom', 'bom');
+    //comando de repetição while
+    $status = array('ótimo', 'Muito bom', 'bom');
 
-        foreach ($status as $valor) {
-            echo '$valor <br>';
-        }
+    foreach ($status as $valor) {
+        echo '$valor <br>';
+    }
 ?>
