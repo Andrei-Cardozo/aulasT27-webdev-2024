c4c7b8a5fbb15e99f899f55a0330eef66a4c302f
Andrei Cardozo <andrei.cardozo@unidavi.edu.br>
2024-10-03 21:15:06 -0300

    connectphp

--- aula11/conect SGBD/conect.php
+++ aula11/conect SGBD/conect.php
@@ -1,24 +1,22 @@
 <?php
-try {
- 
-    /*etapa 1 - criar instância de conexão no SGBD (DBname é o nome do database, user= usuario, password, é a senha padrão no caso daqui dos labs é postgres*/
-    $dbconn = pg_connect ("host=localhost port=5432 dbname=postgres user=postgres password=postgres");
+
+    try {
+        $dbconn = pg_connect("host=localhost port=5432 dbname=php user=postgres password=postgres");
+        
         if($dbconn) {
-
-   $aDados = array($_POST['campo_primeiro_nome'],
-                   $_POST['campo_sobrenome'],
-                   $_POST['campo_email'],
-                   $_POST['campo_password'],
-                   $_POST['campo_cidade'],
-                   $_POST['campo_estado']);
-
-
-    $result = pg_query_params($dbconn, "INSERT INTO TBPESSOA (PESNOME, PESSOBRENOME, PESEMAIL, PESPASSWORD, PESCIDADE, PESESTADO)
-    VALUES ($1, $2, $3, $4, $5, $6)",
-    $aDados);               
+            $aDados = array ($_POST['campo_primeiro_nome'],
+                             $_POST['campo_sobrenome'],
+                             $_POST['campo_email'],
+                             $_POST['campo_password'],
+                             $_POST['campo_cidade'],
+                             $_POST['campo_estado']);
+            $result = pg_query_params($dbconn, "INSERT INTO TBPESSOA(PESNOME, PESSOBRENOME, PESEMAIL, PESPASSWORD, PESCIDADE, PESESTADO) VALUES ($1, $2, $3, $4, $5, $6)", $aDados);
+            while ($row = pg_fetch_assoc($result)) {
+                echo print_r($row) . "<br>";
+            }               
         }
-    
     } catch (Exception $e){
         echo $e->getMessage();
     }
+
 ?>
